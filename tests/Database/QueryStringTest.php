f536106714f99c35f4000e40a9cbca623d95ead9
Luis Guillen <luisguillen200592@gmail.com>
2018-06-28 23:07:17 -0500

    cambiar pruebas

--- tests/Database/QueryStringTest.php
+++ tests/Database/QueryStringTest.php
@@ -19,34 +19,37 @@ class QueryStringTest extends TestCase
      */
     public function testSimpleSelectQuery($pdo)
     {
-        $query = Query::instance()->table('users')->select('a, b')->query();
+        $query = Query::table('users')->select('a, b')->__toString();
         $this->assertEquals('select a, b from users', $query);
 
-        $query = Query::instance()
-            ->table('users')
+        $query = Query::table('users')
             ->select('domains.*')
             ->join('domains', 'users.domain_id', '=', 'domains.id')
-            ->query();
+            ->__toString();
         $this->assertEquals('select domains.* from users join domains on users.domain_id = domains.id', $query);
 
-        $query = Query::instance()
-            ->table('users')
+        $query = Query::table('users')
             ->select('domains.*')
             ->join('domains', 'users.domain_id', '=', 'domains.id')
             ->where('domains.name', '=', 'name')
-            ->query();
+            ->__toString();
         $this->assertEquals("select domains.* from users join domains on users.domain_id = domains.id where domains.name = name", $query);
 
-        $query = Query::instance()
-            ->table('users')
+        $query = Query::table('users')
             ->select('domains.*')
             ->join('domains', 'users.domain_id', '=', 'domains.id')
-            ->join('paragraph', 'domains.id', '=', 'paragraph.domain_id')
+            ->leftJoin('paragraph', 'domains.id', '=', 'paragraph.domain_id')
             ->where('domains.name', '=', 'name')
-            ->query();
-        $this->assertEquals("select domains.* from users 
-            join domains on users.domain_id = domains.id 
-            join domains on domains.id = paragraph.domain_id 
-            where domains.name = name", $query);
+            ->__toString();
+        $this->assertEquals("select domains.* from users join domains on users.domain_id = domains.id left join paragraph on domains.id = paragraph.domain_id where domains.name = name", $query);
+
+        $query = Query::table('users')
+            ->select('domains.*')
+            ->join('domains', 'users.domain_id', '=', 'domains.id')
+            ->leftJoin('paragraph', 'domains.id', '=', 'paragraph.domain_id')
+            ->where('domains.name', '=', 'name')
+            ->orWhere('domains.name', '=', 'name2')
+            ->__toString();
+        $this->assertEquals("select domains.* from users join domains on users.domain_id = domains.id left join paragraph on domains.id = paragraph.domain_id where domains.name = name or domains.name = name2", $query);
     }
 }
